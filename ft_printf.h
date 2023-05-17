0f86f6c812aff931ca3df2ff2c50055844dee0e3
Clemence Leblais <cleblais@c3r2p4.42nice.fr>
2023-05-17 16:49:34 +0200

    ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleblais <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 17:22:57 by cleblais          #+#    #+#             */
+/*   Updated: 2022/11/17 18:22:19 by cleblais         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+int		ft_printf(const char *str, ...);
+int		ft_check_well_used(const char *str);
+void	ft_put_format(char c, int *count, va_list args);
+void	ft_putchar_c(char c, int *count);
+void	ft_putstr_s(char *s, int *count);
+void	ft_putnbr_p(void *p, int *count);
+void	ft_putnbr_i(int nb, int *count);
+void	ft_putnbr_u(unsigned int nb, int *count);
+void	ft_putnbr_x(unsigned int nb, char c, int *count);
+
+#endif
